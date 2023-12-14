dd67f8c27129fbb193abc9740f5823495164f0ff
idinae <iddinae@gmail.com>
2023-12-14 21:35:18 +0200

    client app initial

---
+++ client/src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import Home from './components/Home.vue';
+
+export default {
+  components: {
+    Home,
+  },
+};
+</script>
+
+<template>
+  <Home />
+</template>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div class="container" style="text-align:center">
+    <img src="../assets/images/img1.jpg" alt="Logo" style="width: 100%; margin: 1rem auto;">
+    <h1>Home</h1>
+
+   
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
